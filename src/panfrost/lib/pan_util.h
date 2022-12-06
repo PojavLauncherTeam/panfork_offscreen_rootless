anonymususer04/panfork-mesa-22

tiler heap decoding

src/panfrost/lib/pan_util.h
@@ -47,6 +47,7 @@
 #define PAN_DBG_LINEAR          0x1000
 #define PAN_DBG_NO_CACHE        0x2000
 #define PAN_DBG_DUMP            0x4000
+#define PAN_DBG_TILER           0x8000
 
 struct panfrost_device;
 

PojavLauncherTeam/panfork_offscreen_rootless

src/panfrost/lib/pan_util.h
@@ -47,6 +47,7 @@
 #define PAN_DBG_LINEAR          0x1000
 #define PAN_DBG_NO_CACHE        0x2000
 #define PAN_DBG_DUMP            0x4000
+#define PAN_DBG_TILER         0x010000
 
 #ifndef NDEBUG
 #define PAN_DBG_OVERFLOW        0x8000